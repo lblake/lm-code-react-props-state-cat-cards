c28d562557cc090264264a7cc2f4f83239b4a5f7
lblake <treleven.lloyd@gmail.com>
2023-01-29 16:33:40 +0000

    completed the second activity

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import { useState } from 'react';
 import Navbar from './components/navbar';
 import Header from './components/header';
 import Footer from './components/footer';
+import CatCard from './components/cat_card';
 import Cat from './data/cat';
-import {CatData} from './data/cat-data';
+import { CatData } from './data/cat-data';
 
 function App(): JSX.Element {
   const [cats, setCats] = useState<Array<Cat>>(CatData);
 
+//   console.log("Our pretties 😻: ", cats)
+
+
   return (
+	
     <>
       <Navbar />
       <Header />
+	  
 
       <main>
         <div className='cards__wrapper'>{/* JSX code can go here */}</div>
