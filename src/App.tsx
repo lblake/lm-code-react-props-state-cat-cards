dacfdf7134cde6884b956b73d43a42ad181f957d
lblake <treleven.lloyd@gmail.com>
2023-01-29 16:16:30 +0000

    completed first activity, also created a catData file to hold the array of cat information

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 import Navbar from './components/navbar';
 import Header from './components/header';
 import Footer from './components/footer';
+import Cat from './data/cat';
+import {CatData} from './data/catData';
 
 function App(): JSX.Element {
+  const [cats, setCats] = useState<Array<Cat>>(CatData);
 
-	// JavaScript/TypeScript code can be inserted here!
+  return (
+    <>
+      <Navbar />
+      <Header />
 
-	return (
-		<>
-			<Navbar />
-			<Header />
+      <main>
+        <div className='cards__wrapper'>{/* JSX code can go here */}</div>
+      </main>
 
-			<main>
-				<div className='cards__wrapper'>{/* JSX code can go here */}</div>
-			</main>
-
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 }
 
 export default App;
